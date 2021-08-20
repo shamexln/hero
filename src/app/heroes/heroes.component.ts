0e9d553f4452bf487e5c439cf83a3322d132eb71
Lei Shao <Lei.Shao@elekta.com>
2021-08-20 14:49:35 +0800

    Add route

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,13 +9,14 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  constructor() { }
+  heroes:Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
-
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes=>this.heroes = heroes);
+  }
 }
